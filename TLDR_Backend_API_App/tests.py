55eb76fa94a05c74a699f14c1f308c173ad476ca
Conner Van Loan <cdvanloan@gmail.com>
2023-05-12 15:21:01 -0700

    adds circleci folder and cnofig

--- TLDR_Backend_API_App/tests.py
+++ TLDR_Backend_API_App/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from TLDR_Backend_API_App.models import MyModel
 
-# Create your tests here.
+class MyModelTestCase(TestCase):
+    def setUp(self):
+        MyModel.objects.create(name="test1")
+        MyModel.objects.create(name="test2")
+
+    def test_MyModel(self):
+        """MyModel are correctly identified"""
+        test1 = MyModel.objects.get(name="test1")
+        test2 = MyModel.objects.get(name="test2")
+
+        self.assertEqual(test1.name, "test1")
+        self.assertEqual(test2.name, "test2")
