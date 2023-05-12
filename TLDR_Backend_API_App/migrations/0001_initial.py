449400593ed3577f1d6927aa5b6611af0ee961b9
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-05-12 08:45:47 -0600

    create development environment

--- TLDR_Backend_API_App/migrations/0001_initial.py
+++ TLDR_Backend_API_App/migrations/0001_initial.py
@@ -15,7 +15,7 @@ class Migration(migrations.Migration):
             name='MyModel',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('name', models.CharField(max_length=200)),
+                ('name', models.CharField(max_length=200))
             ],
         ),
     ]
