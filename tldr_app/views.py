57a20f1d0f8a7dc30f2db83dc95c951bd4709dac
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-05-14 11:01:16 -0600

    add initiate_query method, delete storage

--- tldr_app/views.py
+++ tldr_app/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets, generics, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import *
-from .serializers import UserSerializer, QuerySerializer
+from .serializers import UserSerializer, QuerySerializer, ResultSerializer
 from .renderers import CustomJSONRenderer
 from .services import *
 
@@ -42,9 +42,14 @@ class QueryApiView(APIView):
       return Response(serializer.data, status=status.HTTP_200_OK)
                 
     def post(self, request, *args, **kwargs):
+      query = Query(user=User.objects.get(id=request.data['user']), areas_of_focus=request.data['areas_of_focus'], tos=request.data['tos'])
+      query.save()
+      result = Result(response=QueryGPT.initiate_query(query.tos), query=query)
+      result.save()
+      serializer = ResultSerializer(result, many=False)
+      # if serializer.is_valid():
+      # serializer.save()
       import pdb; pdb.set_trace()
-      serializer = QuerySerializer(data=request.data)
-      if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
+      # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+      # return Response("string", status=status.HTTP_400_BAD_REQUEST)
