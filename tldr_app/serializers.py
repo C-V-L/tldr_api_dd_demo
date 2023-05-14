7d619ac520d90f76078d136a00e3bef91f531775
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-05-14 11:01:16 -0600

    add initiate_query method, delete storage

--- tldr_app/serializers.py
+++ tldr_app/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import User, Query
+from .models import *
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +10,8 @@ class QuerySerializer(serializers.ModelSerializer):
 		class Meta:
 				model = Query
 				fields = ['id', 'user', 'areas_of_focus', 'tos']
+
+class ResultSerializer(serializers.ModelSerializer):
+		class Meta:
+				model = Result
+				fields = ['id', 'response', 'query']
--- tldr_app/services.py
+++ tldr_app/services.py
@@ -1,58 +1,16 @@
 import os 
-import pickle
 from decouple import config
-from google.auth.transport.requests import Request
-from google_auth_oauthlib.flow import InstalledAppFlow
-from llama_index import GPTVectorStoreIndex, SimpleDirectoryReader, download_loader, GPTListIndex, LLMPredictor, PromptHelper
-from llama_index import ServiceContext, StorageContext, load_index_from_storage
+from llama_index import GPTVectorStoreIndex, SimpleDirectoryReader
 from .models import *
 
-
-# import pdb; pdb.set_trace()
-# PromptHelper.set_openai_key(os.environ['OPENAI_API_KEY'])
-
-class QueryGPT():
-	def authorize_gdocs():
-		google_oauth2_scopes = [
-			"https://www.googleapis.com/auth/documents.readonly"
-		]
-		cred = None
-		if os.path.exists("token.pickle"):
-			with open("token.pickle", 'rb') as token:
-				cred = pickle.load(token)
-		if not cred or not cred.valid:
-			if cred and cred.expired and cred.refresh_token:
-				cred.refresh(Request())
-			else:
-				flow = InstalledAppFlow.from_client_secrets_file("credentials.json", google_oauth2_scopes)
-				cred = flow.run_local_server(port=3000)
-			with open("token.pickle", 'wb') as token:
-				pickle.dump(cred, token)
-
-
-# function to authorize or download latest credentials 
-# authorize_gdocs()
-	import pdb; pdb.set_trace()
-# initialize LlamaIndex google doc reader
-	def get_documents(query):
-		GoogleDocsReader = download_loader('GoogleDocsReader')
-
-	# list of google docs we want to index 
-		gdoc_ids = ['1dZgdb8vqtJ015v3y9e89eYcBhqWbh8vq041FJqwoZso']
-
-		loader = GoogleDocsReader()
-
-	# load gdocs and index them 
-		documents = loader.load_data(gdoc_ids)
-
-		return documents
-# index = GPTVectorStoreIndex.from_documents(documents=documents)
-# import pdb; pdb.set_trace()
-# index.storage_context.persist(persist_dir = 'storage')
-
-
-# storage_context = StorageContext.from_defaults(persist_dir = 'storage')
-# index = load_index_from_storage(storage_context)
-# query_engine = index.as_query_engine()
-
-# response = query_engine.query("I have provided you two terms of service for two different terms of service for a music streaming application. Can you compare and contrast the two in substantial detail?")
+class QueryGPT():	
+	def initiate_query(query):
+		os.environ['OPENAI_API_KEY'] = config('OPENAI_API_KEY')
+		file = open('/Users/axeld/Documents/turing/4mod/projects/tldr_api/data/tos.txt', 'w')
+		file.write(query)
+		file.close()
+		documents = SimpleDirectoryReader('data').load_data()
+		index = GPTVectorStoreIndex.from_documents(documents=documents)
+		query_engine = index.as_query_engine()
+		response = query_engine.query("I am providing you the terms and services of a company and I am seeking information about how this would apply to me. Am I allowed to assign my rights under this agreement to anyone else without the other party's consent?")
+		return response
