63a9b7ead298c9b416d1bbb0c24128f4ba6f0ada
Hady Matar <hadyematar@gmail.com>
2023-05-15 10:12:57 -0600

    Hady Tests

--- tldr_app/services.py
+++ tldr_app/services.py
@@ -12,6 +12,8 @@ class QueryGPT():
 		documents = SimpleDirectoryReader('data').load_data()
 		index = GPTVectorStoreIndex.from_documents(documents=documents)
 		query_engine = index.as_query_engine()
-		# Possibly will want to make this an array that is iterated over... each item is a separate query to the ChatGPT database. 
-		response = query_engine.query(f"I am providing you the terms and services of a company and I am seeking information about how this would apply to me. Here are the two areas of focus I am concerned about; they will be in an array format but they are two separate and distinct issues. Please provide me it in the following format. The focus then a colon and then any relevant bullet points on that issue. Please also include information on how I, as a consumer, can protect my interests and rights in these regards.{query.areas_of_focus}")
-		return response
+		responses = []
+		for area_of_focus in query.areas_of_focus:
+			response = query_engine.query(f"I am providing you the terms and services of a company and I am seeking information about how this would apply to me. I am concerned about this area: {area_of_focus}. Please provide me the following information in the following format: AREA OF FOCUS: {area_of_focus} REQUEST 1: information about how this terms of service would effect me with regards to {area_of_focus}. REQUEST 2: any steps outlined in the terms of service how I, as the consumer can take to protect myself in regards to the issue of {area_of_focus}. REQUEST 3: On a scale of 1 to 10, with 1 being the most business friendly and 10 being the most consumer friendly, how doe this terms of service compare on the issue of {area_of_focus} with that of the industry standard. Do not include complete sentences such as 'on a scale of 1 to 10, this is a 7.' in that case, just respond 7. You need to be succinct.I want the formatting to be exactly like I outlined above, with you including the capital letters of what I have provided in the prompt and the lowercase letters to be your response. Make sure the entire response is 200 words/ 1300 characters or less. Please only include the information I am specifically requesting. Do not include anything extraneous and do not quote extensively from the terms.")
+			responses.append(str(response))
+		return responses
