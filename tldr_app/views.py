bcf86a7738267a029edc8baf7de3d268f58e3e21
Axel De La Guardia <axeldelaguardia@gmail.com>
2023-05-18 20:53:26 -0600

    add: ability to read pdf, parse through it, and create query in query post request

--- tldr_app/views.py
+++ tldr_app/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
-from rest_framework import viewsets, generics, status
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.parsers import MultiPartParser, JSONParser
 from .models import *
 from .serializers import UserSerializer, QuerySerializer, ResultSerializer
 from .renderers import CustomJSONRenderer
 from .services import *
+import PyPDF2
 
 class HealthCheckView(View):
 	def get(self, request):
@@ -27,23 +29,41 @@ class UserApiView(APIView):
 			serializer.save()
 			return Response(serializer.data, status=status.HTTP_201_CREATED)
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-	      
+
 
 class QueryApiView(APIView):
 	renderer_classes = [CustomJSONRenderer]
-	
+	parser_classes = [MultiPartParser, JSONParser]
+
 	def get(self, request, *args, **kwargs):
 		queries = Query.objects.all()
 		serializer = QuerySerializer(queries, many=True)
 		return Response(serializer.data, status=status.HTTP_200_OK)
-							
+
 	def post(self, request, *args, **kwargs):
-		# import pdb; pdb.set_trace()
-		query_serializer = QuerySerializer(data=request.data)
+		if request.FILES:
+			query_serializer = QuerySerializer(data=process_request_data(request.data))
+		else:
+			query_serializer = QuerySerializer(data=request.data)
+
 		if query_serializer.is_valid():
-			query = query_serializer.save()      
+			query = query_serializer.save()
 			results = QueryGPT.initiate_query(query)
 			serializer = ResultSerializer(results, many=True)
 			return Response(serializer.data, status=status.HTTP_201_CREATED)
 		else:
 			return Response(query_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+def process_uploaded_file(file):
+	pdf_reader = PyPDF2.PdfReader(file)
+	combined_content = ""
+	for page in pdf_reader.pages:
+		page_content = page.extract_text()
+		combined_content += page_content
+	return combined_content
+
+def process_request_data(data):
+	data['tos'] = process_uploaded_file(data['file'])
+	data['user'] = int(data.get('user'))
+	del data['file']
+	return data
