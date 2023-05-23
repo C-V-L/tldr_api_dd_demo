c1b0974a7b77d11fcdfa0ab8480aee15f067f3c5
Conner Van Loan <cdvanloan@gmail.com>
2023-05-23 12:22:39 -0600

    adds working try/expect for bad chatgpt response

--- tldr_app/services.py
+++ tldr_app/services.py
@@ -14,7 +14,10 @@ class QueryGPT():
     json_string = query_response_format()
     for area_of_focus in query.areas_of_focus:
       response = query_engine.query(f"I am providing you the terms and services of a company. You are instructed to read these terms of service and to respond to the query I have in regard to the terms of service. My query is: How does this terms of service address {area_of_focus}. Please respond with the information in the format of a json string like this: {json_string} where the key 'area_of_focus' is {area_of_focus}. Please make sure that the formatting of the json string is correct and there are no additional single or double quotes other than what a json string would require")
-      result = Result(response=json.loads(str(response)), query=query)
+      try:
+        result = Result(response=json.loads(str(response)), query=query)
+      except:
+        result = Result(response=str(error_response(area_of_focus)), query=query)
       result.save()
       responses.append(result)
     return responses
@@ -39,4 +42,15 @@ def query_response_format():
         "ranking": "On a scale of 1 to 10, with 1 being the most business friendly and 10 being the most consumer friendly, how does this terms of service compare on the issue of this area_of_focus with that of the industry standard. Do not include complete sentences such as 'on a scale of 1 to 10, this is a 7.' in that case, just respond 7. You need to be succinct."
       }
      """
+  return format
+
+def error_response(focus):
+  format = """
+      {
+				"title": "%s",
+        "impact": "We were unable to get our pal ChatGPT to do something useful, please try again.",
+        "actionable": "Please try again",
+        "ranking": "n/a"
+      }
+      """ % focus
   return format
--- tldr_app/tests/test_query_gpt.py
+++ tldr_app/tests/test_query_gpt.py
@@ -2,10 +2,16 @@ import pytest
 from tldr_app.models import *
 from tldr_app.services import QueryGPT, query_to_file
 import vcr
+from unittest.mock import patch, MagicMock
+import json
 
 my_vcr = vcr.VCR(
     filter_headers=['Authorization'],
 )
+# def test_create_query_engine(db):
+#   query_gpt = QueryGPT()
+#   query_engine = query_gpt.create_query_engine()
+#   assert isinstance(query_engine, object)
 
 def test_initiate_query(db):
   query_gpt = QueryGPT()
@@ -24,6 +30,33 @@ def test_initiate_query(db):
     assert isinstance(first_list_element.response['impact'], str)
     assert isinstance(first_list_element.response['actionable'], str)
     assert isinstance(first_list_element.response['ranking'], str)
+
+def test_query_response_format_error(db):
+  query_gpt = QueryGPT()
+  random_user = User.objects.create(name="Hady")
+  random_query = Query.objects.create(tos="The Parties agree that all claims and disputes arising under or relating to this Agreement are first to be settled by mediation administered by Arbitration Resolution Services (ARS. The parties expressly agree to abide by the Mediation Rules & Regulations of ARS as found at the ARS website, www.arbresolutions.com. Any unresolved disputes arising under or relating to this Agreement are to be settled by binding arbitration. The arbitration shall be conducted by ARS and the parties shall be bound by any and the applicable arbitration rules of ARS and any award/decision rendered. ARS rules can be found at the ARS website. Any decision or award as a result of any such arbitration proceeding shall be in writing and shall provide an explanation for all decisions. Any such arbitration shall be conducted by an arbitrator experienced in [insert industry or legal experience required for arbitrator]and shall include a written record of the arbitration hearing. An award of arbitration may be confirmed in a court of competent jurisdiction. Unless the parties agree otherwise, the same individual will not serve as both mediator arbitrator.", user=random_user, areas_of_focus= ["mandatory binding arbitration", "focus"])
+  format = """
+        {
+          "title": "areas_of_focus"
+          "impact": "key information about how this terms of service would effect me in regards to this area_of_focus.",
+          "actionable": "any steps outlined in the terms of service how I, as the consumer can take to protect myself in regards to this area_of_focus.',
+          "ranking": "On a scale of 1 to 10, with 1 being the most business friendly and 10 being the most consumer friendly, how does this terms of service compare on the issue of this area_of_focus with that of the industry standard. Do not include complete sentences such as 'on a scale of 1 to 10, this is a 7.' in that case, just respond 7. You need to be succinct.",
+        }
+      """
+  
+  with my_vcr.use_cassette('fixtures/vcr_cassettes/query_gpt_500_error.yaml'):
+    with patch('llama_index.query_engine.retriever_query_engine.RetrieverQueryEngine.query') as mock_response:
+      mock_response.return_value = format
+      response = query_gpt.initiate_query(random_query)
+      json_format = json.loads("""
+      {
+				"title": "focus",
+        "impact": "We were unable to get our pal ChatGPT to do something useful, please try again.",
+        "actionable": "Please try again",
+        "ranking": "n/a"
+      }""")
+      json_response = json.loads(response[1].response)
+      assert json_response == json_format
 
 def test_query_to_file(db):
   random_user = User.objects.create(name="Hady")
